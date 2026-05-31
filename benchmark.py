9804e22bfc7f654973805adaf2bdcc45871a803c
Gabriel <ggalmeida0@gmail.com>
2026-05-31 12:05:18 -0500

    adding uv project and fixing up metrics

--- benchmark.py
+++ benchmark.py
@@ -1,9 +1,11 @@
 import argparse
 import asyncio
 import time
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from itertools import chain
 from os import environ, system
 from threading import Lock, Thread
+from typing import List
 
 from faker import Faker
 from openai import AsyncOpenAI
@@ -36,6 +38,7 @@ class Metrics:
     total_errors: int
     lock: Lock
     total_tokens: int
+    ttfts: List[float] = field(default_factory=list)
 
     def record_request(self):
         with self.lock:
@@ -52,6 +55,14 @@ class Metrics:
             self.inflight -= 1
             self.total_errors += 1
 
+    def increment_total_tokens(self, tokens: int):
+        with self.lock:
+            self.total_tokens += tokens
+
+    def add_ttft(self, ttft: float):
+        with self.lock:
+            self.ttfts.append(ttft)
+
 
 class RequestGenerator:
     def __init__(self, model: str, provider: Provider):
@@ -66,7 +77,7 @@ class RequestGenerator:
         self.tokenizer = Tokenizer(model, provider)
         self.lock = Lock()
         self.metrics = Metrics(
-            started_at=time.time(),
+            started_at=time.perf_counter(),
             inflight=0,
             total_requests=0,
             total_success=0,
@@ -79,11 +90,23 @@ class RequestGenerator:
     async def fire_request(self, input_text: str):
         self.metrics.record_request()
         try:
+            request_start_time = time.perf_counter()
             stream = await self.client.completions.create(
                 model=self.model, prompt=input_text, stream=True
             )
+            total_text = ""
+            has_not_seen_first_token = True
             async for completion in stream:
-                completion.choices
+                chunk_text = "".join(
+                    chain.from_iterable(map(lambda c: c.text, completion.choices))
+                )
+                if chunk_text:
+                    total_text += chunk_text
+                if has_not_seen_first_token and len(total_text) > 0:
+                    self.metrics.add_ttft(time.perf_counter() - request_start_time)
+                    has_not_seen_first_token = False
+            generated_tokens = self.tokenizer.encode(total_text)
+            self.metrics.increment_total_tokens(len(generated_tokens))
             self.metrics.record_success()
         except:
             self.metrics.record_failure()
@@ -105,26 +128,34 @@ class RequestGenerator:
     ):
         while True:
             requests = [
-                self.fire_request(self.faker.text(max_nb_chars=input_num_tokens))
+                self.fire_request(self.generate_tokens(input_num_tokens))
                 for _ in range(max_burst)
             ]
             await asyncio.gather(*requests)
             await asyncio.sleep(cool_down_secs)
 
-    def generate_bursty_traffic(self, inputNumChars: int):
-        asyncio.run(self.generate_traffic(inputNumChars, 10, 10))
+    def generate_bursty_traffic(self, input_num_tokens: int):
+        asyncio.run(self.generate_traffic(input_num_tokens, 10, 10))
 
-    def generate_constant_traffic(self, inputNumChars: int):
-        asyncio.run(self.generate_traffic(inputNumChars, 1, 1))
+    def generate_constant_traffic(self, input_num_tokens: int):
+        asyncio.run(self.generate_traffic(input_num_tokens, 1, 1))
 
     def render_dashboard(self):
         while True:
             with self.lock:
-                runtime = time.time() - self.metrics.started_at
+                runtime = time.perf_counter() - self.metrics.started_at
 
                 error_rate = (
                     self.metrics.total_errors / self.metrics.total_requests * 100
                     if self.metrics.total_requests
+                    else 0.0
+                )
+
+                tokens_per_sec = self.metrics.total_tokens / runtime
+
+                avg_ttft = (
+                    sum(self.metrics.ttfts) / len(self.metrics.ttfts)
+                    if self.metrics.ttfts
                     else 0.0
                 )
 
@@ -133,26 +164,29 @@ class RequestGenerator:
                 print(
                     f"""
             ┌──────────────────────────────────────────────┐
-            │ vLLM Benchmark Harness                       │
+            │ BenchPress Performance Harness               │
             ├──────────────────────────────────────────────┤
             │ Runtime            {runtime:<25}│
             ├──────────────────────────────────────────────┤
             │ In-flight          {self.metrics.inflight:<25}│
             │ Total Requests     {self.metrics.total_requests:<25}│
+            │ Tokens/s       {tokens_per_sec:<25}│
+            │ Avg TTFT (client observed)     {avg_ttft:<25}│
             │ Successes          {self.metrics.total_success:<25}│
             │ Errors             {self.metrics.total_errors:<25}│
             │ Error Rate         {f"{error_rate:.2f}%":<25}│
             └──────────────────────────────────────────────┘
             """.strip()
                 )
-                time.sleep(1)
+            time.sleep(1)
 
 
 if __name__ == "__main__":
     args = arg_parser()
-    request_generator = RequestGenerator(model=args.model, provider=args.provider)
+    provider = Provider(args.provider)
+    request_generator = RequestGenerator(model=args.model, provider=provider)
 
     if args.traffic_type == "constant":
-        request_generator.generate_constant_traffic(args.input_size_chars)
+        request_generator.generate_constant_traffic(args.input_size_tokens)
     elif args.traffic_type == "bursty":
-        request_generator.generate_bursty_traffic(args.input_size_chars)
+        request_generator.generate_bursty_traffic(args.input_size_tokens)
